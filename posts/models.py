b42d8d14f93bef74eb5bd3fec56ad9d79a2b739f
KIM SANGKEUN <lovedr0p@naver.com>
2019-04-12 02:37:48 +0000

    02_CRUD && IMAGE UPLOAD

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     content = models.TextField()
+    image = models.ImageField(blank=True)
     
     def __str__(self):
         return self.content
